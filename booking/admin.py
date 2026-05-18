e092f117abd78f9ca7ca022a52f5617970a9adef
Lana <svetlana.mur.85@gmail.com>
2026-05-18 14:47:22 +0100

    Improve admin panel with list display and filters

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Table, Seat, Booking
 
-# Register your models here.
+
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ['number', 'table', 'status']
+    list_filter = ['status']
+
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = ['number', 'row']
+    list_filter = ['row']
+
+
+admin.site.register(Table, TableAdmin)
+admin.site.register(Seat, SeatAdmin)
+admin.site.register(Booking)
